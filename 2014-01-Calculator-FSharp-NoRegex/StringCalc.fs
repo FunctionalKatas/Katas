69b4b058fc4fb29ed34b9bfdc97232ac186d21d1
Richard Dalton <richard@devjoy.com>
2014-03-14 00:33:20 +0000

    String Calculator in F# Without Regex
    Complete Solution to String Calculator, with no Regular Expressions

---
+++ 2014-01-Calculator-FSharp-NoRegex/StringCalc.fs
@@ -0,0 +1,30 @@
+﻿module StringCalc
+
+    open System
+
+    exception NegativesFound of int[]
+    let DefaultDelimiters = [|",";"\n"|]
+
+    let PreventNegatives nums =
+        match nums |> Array.filter (fun n -> n < 0) with
+        | negatives when negatives.Length > 0 -> raise (NegativesFound negatives)
+        | _ -> nums
+
+    let SumNumbers (expr: string[] * string) =
+        let (delimiters, numbers) = expr
+        numbers.Split (delimiters, StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map Int32.Parse
+        |> PreventNegatives
+        |> Array.filter (fun n -> n <= 1000)
+        |> Array.sum
+
+    let ParseDelimiters (expr: string) =
+        let parts = expr.Split([|'\n'|], 2)
+        let delimiters = parts.[0].Split([|"/";"[";"]"|], StringSplitOptions.RemoveEmptyEntries)
+        (delimiters, parts.[1])
+        
+    let Add expr =
+        match expr with
+        | "" -> 0
+        | _ when expr.StartsWith "//" -> expr |> ParseDelimiters |> SumNumbers
+        | _ -> (DefaultDelimiters, expr) |> SumNumbers
