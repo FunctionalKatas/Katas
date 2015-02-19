b4f99803d44b86a8bffd54d15297042812e44030
andrea <roundcrisis@gmail.com>
2015-02-19 23:28:10 +0000

    a version of heart in f#

---
+++ 2015-02-ASCII-Heart/2015-02-Heart-v1.fsx
@@ -0,0 +1,25 @@
+﻿open System
+let l = "love "
+let repeat n s = String.replicate n s
+
+let triangle (loves:int, decrement:int) =     
+    let rec ptr  loves decrement =        
+        match loves <= 0 with 
+        | true ->()
+        | false -> repeat decrement " " + repeat loves l 
+                   |>Console.WriteLine
+                   ptr  (loves-1) (decrement+2 )    
+    ptr loves decrement
+
+let boob i =    
+    let halfLoveCount = i / 2
+    let rec bo (loves:int) i  =
+        match loves > halfLoveCount with 
+        | true -> ()
+        | false -> repeat i " " + repeat loves l  + repeat (i*3) " " + repeat loves l 
+                   |>Console.WriteLine
+                   bo (loves+1) (i-2)
+    bo 1 (i-2) 
+
+boob 10
+triangle( 10, 1) 
